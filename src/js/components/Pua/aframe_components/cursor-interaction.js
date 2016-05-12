26e074a5e69d5df4bc744de60d55158ff34c459e
Gavin Ching <gavinchingy@gmail.com>
2016-05-12 12:08:48 -0400

    Fix event by returning properly

--- src/js/components/Pua/aframe_components/cursor-interaction.js
+++ src/js/components/Pua/aframe_components/cursor-interaction.js
@@ -23,12 +23,14 @@ AFRAME.registerComponent('cursor-interaction', {
 
    document.addEventListener('touchstart',function start(e){
       console.log("PLEASE BE HERE");
+
+      var node = $(".audio-player").data("audio-node");
+      console.log(node);
       if (node === undefined){
+        console.log('node undefined, not removing listener');
         return;
       }
       document.removeEventListener('touchstart',start,false);
-      var node = $(".audio-player").data("audio-node");
-      console.log(node);
       // document.getElementsByTagName('audio')[0].play();
       // document.getElementsByTagName('audio')[0].pause();
       node.start(0);
